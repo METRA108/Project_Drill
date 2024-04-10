85c69d8629dd561b48384e34740a62623dd4ecc0
METRA108 <metrayilmaz07@gmail.com>
2024-04-10 03:35:52 +0300

    tons of update lets check it
    yes it is

--- scripts/Eye.gd
+++ scripts/Eye.gd
@@ -1,6 +1,6 @@
 extends Sprite2D
 const idle = preload("res://sprites/idle_eye.png")
-const green = preload("res://sprites/green.png")
+const green = preload("res://sprites/green_eye.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +11,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_text_completion_accept"):
-		get_node("/root/Node2D/CharacterBody2D/Eye").set_texture(idle)
+		get_node("/root/Node2D/CharacterBody/Eye").set_texture(idle)
 		
 	if Input.is_action_just_released("ui_text_completion_accept"):
-		get_node("/root/Node2D/CharacterBody2D/Eye").set_texture(green)
+		get_node("/root/Node2D/CharacterBody/Eye").set_texture(green)
 	
 		
 
