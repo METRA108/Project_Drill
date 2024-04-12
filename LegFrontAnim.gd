32a05b11819d7098fe5dbb23f8b419bf4f5fa546
METRA108 <metrayilmaz07@gmail.com>
2024-04-12 21:55:40 +0300

    Legs

---
+++ LegFrontAnim.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+var new_texture = preload("res://sprites/RobotLegStop.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_D):
+		play("legfrontalanim")
+	else:
+		stop()
