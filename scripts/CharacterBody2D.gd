0cabdae619bcd463aec6590fccc800b9f6385480
METRA108 <metrayilmaz07@gmail.com>
2024-04-21 02:02:46 +0300

    bu sefer çalıştı
    niceeee
    baya kolaymış ben malmışım

--- scripts/CharacterBody2D.gd
+++ scripts/CharacterBody2D.gd
@@ -10,33 +10,37 @@ const SPEED = 150.0
 @export var inv: Inv
 	
 func _process(_delta):
-	var vect = rad_to_deg(get_global_mouse_position().angle())
-	var angle_to_player = get_global_mouse_position().angle()
-
-	# slowly changes the rotation to face the angle
-	rotation = move_toward(rotation, angle_to_player, 1)
+	var xx = get_global_mouse_position().x
+	var yy = get_global_mouse_position().y
+	
+	var xminus = xx - position.x
+	var yminus = yy - position.y
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var direction2 = Input.get_axis("ui_up", "ui_down")
-	print(rotation)
 	
-	if (vect < -125) or (vect > 125):
-		get_node("Body").set_texture(StateLeft)
-		$Eye.set_position(Vector2(-6, 0))
+	var vect = rad_to_deg(atan2(xminus, yminus))
+	
+	print(vect)
+	
+	if (vect < 55) and (vect > -55):
+		get_node("Body").set_texture(StateFront)
+		$Eye.set_position(Vector2(0, 0))
 		
-	if (vect > -55) and (vect < 55):
+	if (vect > 55) and (vect < 125):
 		get_node("Body").set_texture(StateRight)
 		$Eye.set_position(Vector2(6, 0))
 		
-	if (vect < -55) and (vect > -125):
+	if (vect < -125) or (vect > 125):
 		get_node("Body").set_texture(StateBack)
 		$Eye.visible = false
 	else:
 		$Eye.visible = true
-	
-	if (vect > 55) and (vect < 125):
-		get_node("Body").set_texture(StateFront)
-		$Eye.set_position(Vector2(0, 0))
+		
+	if (vect < -55) and (vect > -125):
+		get_node("Body").set_texture(StateLeft)
+		$Eye.set_position(Vector2(-6, 0))
+		
 		
 	if direction:
 		velocity.x = direction * SPEED
